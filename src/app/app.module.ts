4d86e07d5f81b44a1c7ded9f5c4ee39b24a1bd0f
progblog <info@progblog-web.com>
2022-04-09 15:19:48 +0900

    FORM作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
 
 // Layout
 import { HeaderComponent } from './layouts/header/header.component';
@@ -33,6 +34,7 @@ import { CmsService } from './service/cms.service';
 
 // Other
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -49,6 +51,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ReactiveFormsModule,
 
     // material
     MatButtonModule,
@@ -59,6 +62,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatListModule,
     MatCardModule,
     MatProgressSpinnerModule,
+    MatInputModule
   ],
   providers: [UtilsService, CmsService],
   bootstrap: [AppComponent]
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm = this.fb.group({
+    title: [''],
+    name: [''],
+    email: [''],
+    content: ['']
+  });
+
+  constructor(private fb: FormBuilder) {
+
+  }
 
   ngOnInit(): void {
   }
+
+onSubmit() {
+  console.log(this.contactForm.status);
+}
 
 }
